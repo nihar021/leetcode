3d116509b5d3494170ee4e15cb679c4468d0136d
nihar021 <159137436+nihar021@users.noreply.github.com>
2024-07-31 15:42:59 +0530

    Time: 7 ms (44.59%), Space: 18.1 MB (22.2%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+    int n =  prices.size();
+    // Create a DP table to memoize results
+    vector<vector<int>> dp(n + 1, vector<int>(2, -1));
+
+    // Base condition: When we have no stocks left, the profit is 0.
+    dp[n][0] = dp[n][1] = 0;
+
+    int profit;
+
+    // Loop through the arrsay in reverse order
+    for (int ind = n - 1; ind >= 0; ind--) {
+        for (int buy = 0; buy <= 1; buy++) {
+            if (buy == 0) { // We can buy the stock
+                profit = max(0 + dp[ind + 1][0], -prices[ind] + dp[ind + 1][1]);
+            }
+
+            if (buy == 1) { // We can sell the stock
+                profit = max(0 + dp[ind + 1][1], prices[ind] + dp[ind + 1][0]);
+            }
+
+            dp[ind][buy] = profit;
+        }
+    }
+
+    // The maximum profit is stored in dp[0][0] after all calculations
+    return dp[0][0]; 
+    }
+};
